d00ca74e7d19f28eaf16cb8a88dcd40d14a08c2c
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-05 16:43:32 +0400

    visual comparison must not be the same

--- python.py
+++ python.py
@@ -28,23 +28,25 @@ scissors = '''
 images = [rock, paper, scissors]
 
 user = int(input("What do you choose? 0 for Rock, 1 for Paper 1 2 for Scissors -- "))
-print("User chose")
-print(images[user])
-
-computer = random.randint(0, 2)
-print("Computer chose")
-print(images[computer])
-
 if user >= 3 or user < 0:
     print("Invalit Number")
-elif user == 0 and computer == 2:
-    print("User win!")
-elif computer == 0 and user == 2:
-    print("User lose")
-elif computer > user:
-    print("User lose")
-elif computer < user:
-    print("User win")
-elif user == computer:
-    print("It is a draw")
+else:
+    print("User chose")
+    print(images[user])
+
+    computer = random.randint(0, 2)
+    print("Computer chose")
+    print(images[computer])
+
+
+    if user == 0 and computer == 2:
+        print("User win!")
+    elif computer == 0 and user == 2:
+        print("User lose")
+    elif computer > user:
+        print("User lose")
+    elif computer < user:
+        print("User win")
+    elif user == computer:
+        print("It is a draw")
 
