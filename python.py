33ae246e0b9257fc10e5627bd9f3da97af89bf4f
rufatbayramov90 <rufatbyrmv@gmail.com>
2023-10-05 16:30:50 +0400

    an invalid number status

--- python.py
+++ python.py
@@ -30,7 +30,9 @@ user = int(input("What do you choose? 0 for Rock, 1 for Paper 1 2 for Scissors -
 computer = random.randint(0, 2)
 print(f"Computer choose {computer}")
 
-if user == 0 and computer == 2:
+if user >= 3 or user < 0:
+    print("Invalit Number")
+elif user == 0 and computer == 2:
     print("User win!")
 elif computer == 0 and user == 2:
     print("User lose")
@@ -40,5 +42,4 @@ elif computer < user:
     print("User win")
 elif user == computer:
     print("It is a draw")
-else:
-    print("Invalit Number")
+
